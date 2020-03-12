cce250fc1e6d244dfa351e173225f69bdd9f7efa
Kostiantyn Lysenko <61736005+kosslus@users.noreply.github.com>
2020-03-12 21:07:46 +0800

    Update hw4.3.py

--- hw4.3.py
+++ hw4.3.py
@@ -3,22 +3,22 @@ import random
 log = input('Your login: ')
 
 a = {
-    'login0': ['password', random.randint(0, 500)],
-    'login1': ['password', random.randint(0, 500)],
-    'login2': ['password', random.randint(0, 500)],
-    'login3': ['password', random.randint(0, 500)],
-    'login4': ['password', random.randint(0, 500)],
-    'login5': ['password', random.randint(0, 500)],
-    'login6': ['password', random.randint(0, 500)],
-    'login7': ['password', random.randint(0, 500)],
-    'login8': ['password', random.randint(0, 500)],
-    'login9': ['password', random.randint(0, 500)],
+    'login0': ['password', random.randint(0, 10000)],
+    'login1': ['password', random.randint(0, 10000)],
+    'login2': ['password', random.randint(0, 10000)],
+    'login3': ['password', random.randint(0, 10000)],
+    'login4': ['password', random.randint(0, 10000)],
+    'login5': ['password', random.randint(0, 10000)],
+    'login6': ['password', random.randint(0, 10000)],
+    'login7': ['password', random.randint(0, 10000)],
+    'login8': ['password', random.randint(0, 10000)],
+    'login9': ['password', random.randint(0, 10000)],
 }
 
 if log not in a:
 
     password = input(' Wrong! Generate new: ')
-    a.setdefault(log, [password, random.randint(0, 500)])
+    a.setdefault(log, [password, random.randint(0, 10000)])#?
 
     print('New', log)
 
