34afe1196431a7a17622e47db0aca51c29a7e556
Kostiantyn Lysenko <61736005+kosslus@users.noreply.github.com>
2020-03-12 12:46:28 +0200

    Update hw4.1.1.1.py

--- hw4.1.1.1.py
+++ hw4.1.1.1.py
@@ -1,19 +1,20 @@
 import math
-number1 = int(input('input first number: ')) #choose your number
-sign = input('input "+" or "-" or "*" or "/" or "**" or "sqrt" : ')  #choose symbol
-number2 = int(input('input second number: '))
-if sign in ('+', '-', "*", "/", '**',"sqrt"):
- if sign == '+' or sign == '-' or sign == "*" or sign == "**" or sign == "sqrt":
-     if sign == '+':
-         print(number1 + number2)
-         if sign == '*':
-             print(number1 * number2)
-             if sign == "/":
-                 print(number1 / number2)
-                 if sign == "**":
-                     print(number1 ** number2)
+x = float(input("Number one:"))
+y = float(input("Number two:"))
+operation = input("Operation:")
+result = None
 
-     else:
-         print(math.sqrt (number1/number2))
- else:
-     print('Error')
+if operation == '+':
+    result = x + y
+elif operation == '-':
+    result = x - y
+elif operation == '*':
+    result = x * y
+elif operation == '/':
+    result = x / y
+elif operation == '**':
+    result = x ** y
+elif operation == '^': #anything
+    result = math.sqrt(x/y)
+if result is not None:
+    print(result)
